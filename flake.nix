1315fba5cce124aa55a203ed98478eace405272a
Thomas Collerton <tho.collerton@gmail.com>
2026-08-04 18:28:15 +0200

    add nix-gaming flake

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware = {
-      url = "github:tommasie/nixos-hardware/framework-13-ai-300-mt7925";
+      url = "path:/home/thomas/projects/nixos-hardware/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     nix-amd-ai.url = "github:noamsto/nix-amd-ai";
+    nix-gaming.url = "github:fufexan/nix-gaming";
   };
   outputs =
     inputs@{
@@ -20,13 +21,13 @@
     }:
     {
       nixosConfigurations.fw13 = nixpkgs.lib.nixosSystem {
-        # system = "x86_64-linux";
         modules = [
           ./configuration.nix
           nixos-hardware.nixosModules.framework-amd-ai-300-series
           { nixpkgs.hostPlatform = "x86_64-linux"; }
           sops-nix.nixosModules.sops
           inputs.nix-amd-ai.nixosModules.default
+          inputs.nix-gaming.nixosModules.wine
         ];
       };
     };
